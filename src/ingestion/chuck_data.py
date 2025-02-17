cfb87da6c686ce627cf0307f9a85485eee6a0e65
Shubham <shubhamlipare33@gmail.com>
2025-02-17 17:53:52 +0530

    Adding logging and try-exception to load and chuck files

--- src/ingestion/chuck_data.py
+++ src/ingestion/chuck_data.py
@@ -1,5 +1,15 @@
 from langchain.text_splitter import RecursiveCharacterTextSplitter
+import sys
+from src.logger import logging
+from src.exception import CustomException
 
 def chuck_text(text,chunk_size=512,overlap=100):
-    splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=overlap)
-    return splitter.split_text(text)
+
+    try:
+        logging.info("Text splitting has begun")
+        splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=overlap)
+
+        logging.info("Returning splitted text")
+        return splitter.split_text(text)
+    except Exception as e:
+        raise CustomException(e,sys)
--- src/ingestion/load_data.py
+++ src/ingestion/load_data.py
@@ -1,8 +1,19 @@
 import PyPDF2
+import sys
+from src.logger import logging
+from src.exception import CustomException
 
 def load_pdf(path):
-    with open(path,"rb") as file_path:
-        reader=PyPDF2.PdfFileReader(file_path)
-        text="".join([page.extract_text() for page in reader])
 
-    return text
+    try:
+        logging.info("Loading data from source")
+        with open(path,"rb") as file_path:
+            reader=PyPDF2.PdfFileReader(file_path)
+            text="".join([page.extract_text() for page in reader])
+
+        logging.info("Data loaded sucessfully, returning text")
+        return text
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+
